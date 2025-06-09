52b02ec7532912b8fbee4a643ac5dc1b49778bf0
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-06-09 04:46:14 +0200

    + base command for private / custom command

---
+++ synapsex/commands/your_command.py
@@ -0,0 +1,8 @@
+# synapsex/commands/your_command.py
+class Command:
+    name = "yourcommand"
+    desc = "Your description"
+    usage = "yourcommand <args>"
+
+    def run(self, args):
+        print("Logic here")
